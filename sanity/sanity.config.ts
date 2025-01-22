53e5dfb56458aa450ec7d92b435850fab595e540
Hashir Irshad <khanjarquantum@gmail.com>
2025-01-22 08:11:52 +0500

    Front-End Implemntation

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -9,9 +9,9 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId, token} from './sanity/env'
-import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+import {apiVersion, dataset, projectId, token} from './env'
+import {schema} from './schemaTypes'
+import {structure} from './structure'
 
 export default defineConfig({
   basePath: '/studio',
